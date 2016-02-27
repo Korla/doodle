f669f6183156a9e835444cb28f725f01eda02eb2
korla <johannes.bengtsson@raysearchlabs.com>
2016-02-27 21:50:24 +0100

    Set up state and click streams

--- src/index.js
+++ src/index.js
@@ -8,39 +8,53 @@ import {r6} from './data';
 
 import Rx from 'rx';
 import Cycle from '@cycle/core';
-import {makeDOMDriver, h} from '@cycle/dom';
+import {makeDOMDriver, h, svg} from '@cycle/dom';
 
-function main(drivers) {
+function main(sources) {
   var data = getData(r6);
-  var points = data.filter(p => p.exists);
-  var pointLines = getPointLines(data.filter(p => p.exists));
-  var crossLines = getCrossLines(data.filter(p => !p.exists));
-  var curves = getCurves(data.filter(p => p.exists));
 
-  let state$ = Rx.Observable.just({
-    points,
-    lines: pointLines.concat(crossLines).concat(curves)
-  });
-  let container$ = drivers.DOM.select('.container').observable.map(elem => elem[0]);
-  let view$ = state$
-    .map(state => h('svg', {className: 'container', attributes: { width: '400', height: '400', viewBox: '0 0 100 100' }}));
+  var pointClicks$ = sources.DOM.select('.point').events('click')
+    .map(event => event.target.attributes.data.value.split(','))
+    .map(intString => parseInt(intString, 10));
+
+  var state$ = Rx.Observable.of(r6)
+    .merge(pointClicks$)
+    .scan((state, point) => {
+      var i = (point[1] - 1) * state.rowLength + point[0] - 1;
+      state.points[i] = state.points[i] ? 0 : 1;
+      return state;
+    });
+
+  let data$ = state$
+    .map(data => {
+      var points = getData(data);
+      var pointLines = getPointLines(points.filter(p => p.exists));
+      var crossLines = getCrossLines(points.filter(p => !p.exists));
+      var curves = getCurves(points.filter(p => p.exists));
+      return {
+        points,
+        lines: pointLines.concat(crossLines).concat(curves)
+      }
+    });
+
+  let container$ = sources.DOM.select('.container').observable.map(elem => elem[0]);
 
   return {
-    DOM: view$,
-    pointDriver: Rx.Observable.combineLatest(
-      container$, state$,
-      (container, data) => ({container, data: data.points})),
+    DOM: state$.map(state => svg('svg', {class: 'container', attributes: { width: '400', height: '400', viewBox: '0 0 100 100' }})),
     lineDriver: Rx.Observable.combineLatest(
-      container$, state$,
-      (container, data) => ({container, data: data.lines})),
+      container$, data$.map(data => data.lines),
+      (container, data) => ({container, data})),
+    pointDriver: Rx.Observable.combineLatest(
+      container$, data$.map(data => data.points),
+      (container, data) => ({container, data})),
   };
 }
 
 const drivers = {
   DOM: makeDOMDriver('body'),
   log: (input$) => input$.subscribe(val => console.log(val)),
+  lineDriver: (input$) => input$.subscribe(drawLines),
   pointDriver: (input$) => input$.subscribe(drawPoints),
-  lineDriver: (input$) => input$.subscribe(drawLines)
 };
 
 Cycle.run(main, drivers);
@@ -55,8 +69,12 @@ function drawPoints({container, data}) {
     .append('circle')
     .attr('cx', p => p.pos[0])
     .attr('cy', p => p.pos[1])
-    .attr('r', 0.5)
+    .attr('r', 1)
+    .attr('class', 'point')
+    .attr('stroke-width', 3)
+    .attr('stroke', 'transparent')
     .attr('fill', p => rgbToHex(255, p.pos[0]*2.55, p.pos[1]*2.55))
+    .attr('data', p => p.coords.join(','))
   ;
 }
 
